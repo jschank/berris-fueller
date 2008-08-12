95537255c5d6757675594decf10570e81c5448dd
John F. Schank III <jschank@mac.com>
2008-08-11 23:05:02 -0400

    Added table borders, and current miles

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,11 @@
 class Vehicle < ActiveRecord::Base
   has_many :fill_ups
   
+  def current_miles
+    return fill_ups.maximum(:odometer) unless fill_ups.count == 0
+    return miles
+  end
+  
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
