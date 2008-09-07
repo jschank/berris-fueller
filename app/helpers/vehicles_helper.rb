6cd6d1e5f76353f6f2619c7c8c0a2915f53eb0f5
John F. Schank III <jschank@mac.com>
2008-09-07 03:25:49 -0400

    Added selector for gas grade in form, and converted display from octane number to grade name

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -4,5 +4,14 @@ module VehiclesHelper
     fill_ups = vehicle.fill_ups.sort { |a,b| a.odometer <=> b.odometer}
     render :partial => 'vehicle_fill_up', :locals => {:fill_ups => fill_ups, :vehicle => vehicle  }
   end
+  
+  def grade_description(grade)
+    case grade
+    when 87 then "Regular"
+    when 89 then "Plus"
+    when 91 then "Premium"
+    when 93 then "Ultra"
+    end
+  end
     
 end
