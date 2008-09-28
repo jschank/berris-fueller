9eaa18cd6b4c0e3d55804073d832994c91fe2517
John F. Schank III <jschank@mac.com>
2008-09-28 15:22:19 -0400

    Added flash effects

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function() 
+{
+	// Build tabs...
 	$('#tabs > ul').tabs({ fx: { height: 'toggle', opacity: 'toggle' } });
+	
+	// Animate the hiding of any flash elements...
+  $("div.flash").show("scale", {}, 1000);
+	setTimeout( function(){$("div.flash").hide("scale", {}, 1000);}, 5000);
 });
