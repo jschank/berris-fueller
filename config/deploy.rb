fde54273a745f6199033c827edd04ef2fa52cc4f
John F. Schank III <jschank@mac.com>
2008-10-06 01:54:52 -0400

    added peepcode capistrano recipes, and removed some comments from the capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,6 @@
 # = Application =
 # ===============
 set :application, "berris-fueller"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "/Volumes/Phoenix/RailsApps/#{application}"
 
 # ============
