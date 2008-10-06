74b9a69e4c755ed953c5f76efa9dca874ae294e4
John F. Schank III <jschank@mac.com>
2008-10-06 01:50:34 -0400

    added capistrano configs

---
+++ config/deploy.rb
@@ -0,0 +1,76 @@
+# ===============
+# = Application =
+# ===============
+set :application, "berris-fueller"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/Volumes/Phoenix/RailsApps/#{application}"
+
+# ============
+# = Settings =
+# ============
+default_run_options[:pty] = true
+set :use_sudo, true
+
+# ===========
+# = Servers =
+# ===========
+set :user, "deploy"
+set :domain, "cooper.local"
+#server domain, :app, :web
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+# =======
+# = git =
+# =======
+set :scm, :git
+set :repository,  "git@cooper.local:berris-fueller.git"
+set :scm_username, user
+set :runner, user
+set :branch, "master"
+
+# =========
+# = Rails =
+# =========
+set :rails_env, 'production'
+
+# =============
+# = Passenger =
+# =============
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+# ===========
+# = Mongrel =
+# ===========
+# set :mongrel_servers, 3
+# set :mongrel_port, 8000
+
+# Or, at the top of this recipe:
+#   require 'mongrel_cluster/recipes'
+
+# namespace :deploy do
+#   desc "Restarting mod_rails with restart.txt"
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "touch #{current_path}/tmp/restart.txt"
+#   end
+#  
+#   [:start, :stop].each do |t|
+#     desc "#{t} task is a no-op with mod_rails"
+#     task t, :roles => :app do ; end
+#   end
+# end
+
