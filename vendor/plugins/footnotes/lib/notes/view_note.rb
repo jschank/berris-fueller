ea77d5cd386b36e5312f899c0a0269e06f572f27
John F. Schank III <jschank@mac.com>
2008-10-25 16:48:45 -0400

    Updated rails-footnotes for 2.2.0

--- vendor/plugins/footnotes/lib/notes/view_note.rb
+++ vendor/plugins/footnotes/lib/notes/view_note.rb
@@ -8,41 +8,25 @@ module Footnotes
         @template = controller.instance_variable_get('@template')
       end
 
-      def self.to_sym
-        :view
-      end
-
       def row
         :edit
       end
 
-      def title
-        'View'
-      end
-
       def link
-        escape(Footnotes::Filter.prefix + template_file_name)
+        escape(Footnotes::Filter.prefix + filename)
       end
 
       def valid?
-        template_path && prefix? && @template.respond_to?(:finder)
+        prefix? && first_render?
       end
 
       protected
-        def template_extension(path)
-          @template.finder.pick_template_extension(path)
+        def first_render?
+          @template.__send__(:_first_render)
         end
-
-        def template_base_path(path)
-          @template.finder.pick_template(path, template_extension(path))
-        end
-
-        def template_path
-          @template.first_render
-        end
-
-        def template_file_name
-          File.expand_path(template_base_path(template_path))
+        
+        def filename
+          @filename ||= @template.__send__(:_first_render).filename
         end
     end
   end
